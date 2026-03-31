fac8875eafdfafe6ee65711d846a050b609619d0
박한영 <ryan@hanyoungs-MacBook-Pro.local>
2026-03-31 21:34:28 +0900

    fix: build error 수정

--- src/apis/notion-client/getRecordMap.ts
+++ src/apis/notion-client/getRecordMap.ts
@@ -1,8 +1,7 @@
-import { NotionAPI } from "notion-client"
+import { getNotionPage } from "./notionApi"
 import { normalizeRecordMap } from "src/libs/utils/notion/normalizeRecordMap"
 
 export const getRecordMap = async (pageId: string) => {
-  const api = new NotionAPI()
-  const recordMap = normalizeRecordMap(await api.getPage(pageId) as any)
+  const recordMap = normalizeRecordMap(await getNotionPage(pageId) as any)
   return recordMap
 }
